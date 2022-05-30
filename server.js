83a4ea532f93698bd9cc0807b717717c0a487f7e
Marco Busellato <marco.busellato@develondigital.com>
2022-05-30 16:59:46 +0200

    register public folder

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -5,7 +6,7 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 const { userRoutes } = require('./routes/user')
 app.use(userRoutes)
